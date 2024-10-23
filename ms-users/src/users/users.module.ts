ca49e35821bd0b355a5888deeee34d7d0ae02b3a
SebaBlamey <sebastian.cortes04@alumnos.ucn.cl>
2024-10-23 19:39:22 -0300

    fixed: fixed docker compose

--- ms-users/src/users/users.module.ts
+++ ms-users/src/users/users.module.ts
@@ -14,7 +14,7 @@ import { ClientsModule, Transport } from "@nestjs/microservices";
         name: "USER",
         transport: Transport.RMQ,
         options: {
-          urls: ["amqp://localhost:5672"],
+          urls: ["amqp://rabbitmq:5672"],
           queue: "purchaseQueue",
           queueOptions: {
             durable: true,
